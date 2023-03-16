42d64ed5e795c4ba96793b3845316c6d6bf69103
Dennis Iversen <dennis.iversen@gmail.com>
2023-03-16 23:28:15 +0100

    allow to ignore CLI streams STDIN, STDOUT, STDERR. refactor a bit

--- src/Spinner.php
+++ src/Spinner.php
@@ -17,9 +17,14 @@ class Spinner
     private $blink_on = "\e[?25h";
     private $clear_line = "\33[2K\r";
     private $position_zero = "\r";
+    // Ignore these streams. No spinner.
+    private array $ignore_streams = [STDERR, STDIN];
 
-    public function __construct(string $spinner = 'dots', bool $use_keyboard_interrupts = true)
-    {
+    public function __construct(
+        string $spinner = 'dots',
+        bool $use_keyboard_interrupts = true,
+        array $ignore = [STDERR, STDIN]
+    ) {
         $this->use_keyboard_interrupts = $use_keyboard_interrupts;
         $spinner_json = file_get_contents(__DIR__ . '/spinners.json');
         $spinner_ary = json_decode($spinner_json, true);
@@ -79,29 +84,36 @@ class Spinner
 
         // Output is being displayed on the screen
         // Only start spinner if output is not redirected to a file
-        if (posix_isatty(STDOUT)) {
+        if (!in_array(STDOUT, $this->ignore_streams)) $this->runCallback($callback);
+        if (!in_array(STDERR, $this->ignore_streams)) $this->runCallBack($callback);
 
-            if ($this->use_keyboard_interrupts) {
-                $this->keyboardInterrupts();
-            }
-
-            $pid = pcntl_fork();
-            if ($pid == -1) {
-                throw new Exception('Could not fork process');
-            } else if ($pid) {
-                // Parent process
-                $this->pid = $pid;
-                $res = $callback();
-                $this->interrupt();
-                posix_kill($this->pid, SIGTERM);
-                return $res;
-            } else {
-                // Child process
-                $this->start();
-            }
+        if (!in_array(STDIN, $this->ignore_streams)) {
+            $this->runCallback($callback);
         } else {
             $res = $callback();
             return $res;
+        }
+    }
+
+    public function runCallBack(callable $callback)
+    {
+        if ($this->use_keyboard_interrupts) {
+            $this->keyboardInterrupts();
+        }
+
+        $pid = pcntl_fork();
+        if ($pid == -1) {
+            throw new Exception('Could not fork process');
+        } else if ($pid) {
+            // Parent process
+            $this->pid = $pid;
+            $res = $callback();
+            $this->interrupt();
+            posix_kill($this->pid, SIGTERM);
+            return $res;
+        } else {
+            // Child process
+            $this->start();
         }
     }
 }
